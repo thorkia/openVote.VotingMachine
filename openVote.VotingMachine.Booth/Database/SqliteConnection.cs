b5bf0195d5945c1336b10eacd997039837ae5bbb
Robert Maissan <robert.maissan@gmail.com>
2016-05-21 12:07:23 -0400

    Remove code that deletes the existing votes on every launch.

--- openVote.VotingMachine.Booth/Database/SqliteConnection.cs
+++ openVote.VotingMachine.Booth/Database/SqliteConnection.cs
@@ -9,12 +9,8 @@ namespace openVote.VotingMachine.Booth.Database
 		{
 			get
 			{
+				//TODO: Add code to delete the stored DB if needed as part of the reboot - receive a command: IE: Start new election
 				string path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "db.sqlite");
-
-				if (File.Exists(path))
-				{
-					File.Delete(path);
-				}
 
 				return new SQLiteConnection(new SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path, true);
 			}
