d90f3dd830f51e23ae07a334cf4b3ac9af1907f5
Robert Maissan <robert.maissan@gmail.com>
2016-05-21 12:08:43 -0400

    Add comment to help remember what I planned

--- openVote.VotingMachine.Booth/Database/SqliteConnection.cs
+++ openVote.VotingMachine.Booth/Database/SqliteConnection.cs
@@ -10,6 +10,8 @@ namespace openVote.VotingMachine.Booth.Database
 			get
 			{
 				//TODO: Add code to delete the stored DB if needed as part of the reboot - receive a command: IE: Start new election
+				//Maybe change it to take a name - the election it is for and use that!
+				//Wrap all the ballots in an "Election", that has a name - use that for the file name
 				string path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "db.sqlite");
 
 				return new SQLiteConnection(new SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path, true);
