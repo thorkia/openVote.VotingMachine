0f427bff794011b7eac346c42e0c37a6fd1b3edf
Robert Maissan <robert.maissan@gmail.com>
2016-05-29 15:25:17 -0400

    Add basic level register machine data

---
+++ openVote.VotingMachine.Server/Server/Controllers/RegisterMachineController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Devkoes.Restup.WebServer.Attributes;
+using Devkoes.Restup.WebServer.Models.Schemas;
+using Devkoes.Restup.WebServer.Rest.Models.Contracts;
+
+namespace openVote.VotingMachine.Server.Server.Controllers
+{
+	[RestController(InstanceCreationType.Singleton)]
+	public class RegisterMachineController
+	{
+		private static int _machineCount = 0;
+
+		[UriFormat("/")]
+		public IGetResponse GetRegisterMachine()
+		{
+			//TODO: Register the machine and return the ID
+			_machineCount++;
+
+			return new GetResponse( GetResponse.ResponseStatus.OK, new { ID = _machineCount});
+		}
+	}
+}
---
+++ openVote.VotingMachine.Server/Server/WebServer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Devkoes.Restup.WebServer.Http;
+using Devkoes.Restup.WebServer.Rest;
+using openVote.VotingMachine.Server.Server.Controllers;
+
+namespace openVote.VotingMachine.Server.Server
+{
+	public class WebServer
+	{
+		private readonly HttpServer _server;
+		public WebServer()
+		{
+			_server = new HttpServer(8081);
+
+			var registerRouterHandler = new RestRouteHandler();
+			registerRouterHandler.RegisterController<RegisterMachineController>();
+
+			_server.RegisterRoute("/api/register", registerRouterHandler);			
+		}
+
+		public async void StartServer()
+		{
+			await _server.StartServerAsync();
+		}
+
+		public void StopServer()
+		{
+			_server.StopServer();
+		}
+	}
+}
